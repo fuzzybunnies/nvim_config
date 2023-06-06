add05177a1fedec0ac1e897f675319ac0edfc8da
John Hurst <john.h@autoaccessoriesgarage.com>
2023-06-06 16:24:46 -0400

    turn off format on save for now

--- lua/john/lsp/nullls.lua
+++ lua/john/lsp/nullls.lua
@@ -2,6 +2,8 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 
+-- the on_attach function below is "format on save"
+
 
 null_ls.setup({
   -- these sources eventually try to resolve a binary
@@ -16,19 +18,19 @@ null_ls.setup({
     null_ls.builtins.diagnostics.rubocop,
     null_ls.builtins.formatting.prettier,
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      -- format on save
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- vim.lsp.buf.formatting_sync()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     -- format on save
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --         -- vim.lsp.buf.formatting_sync()
+  --         -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+  --         vim.lsp.buf.format({ bufnr = bufnr })
+  --       end,
+  --     })
+  --   end
+  -- end,
 })
