4e55344558e8a20c0bde1c7b0ff6c2f6f824faf7
John Hurst <John_Hurst@genpt.com>
2024-04-26 17:36:42 -0400

    little rearrange after cold restart

--- init.lua
+++ init.lua
@@ -2,21 +2,24 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require "john.mason"
-
 require "john.options"
 require "john.keymaps"
 require "john.plugins"
 require "john.colorscheme"
+require "john.gitsigns"
+require "john.toggleterm"
+require "john.nvimtree"
+require "john.indent_blankline"
+
+
+
+
 -- require "john.cmp"
 -- require "john.telescope"
 -- require "john.treesitter"
 -- require "john.autopairs"
-require "john.gitsigns"
-require "john.toggleterm"
 -- require "john.icons"
-require "john.nvimtree"
 -- require "john.lsp.nullls"
-require "john.indent_blankline"
 -- require "john.lualine"
 -- require "john.session_manager"
 -- require "john.formatter"
